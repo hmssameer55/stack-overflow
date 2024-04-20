8614790b2d1f555b14e9aca0b6d3172565720b4d
hmssameer55 <hmssameer55@gmail.com>
2024-04-20 20:21:52 +0530

    Add utility function for merging class names in utils.ts

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const page = () => {
   return (
     <div>
-      <SignedIn>
-        <UserButton afterSignOutUrl="/" />
-      </SignedIn>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
+     
     </div>
   );
 };
